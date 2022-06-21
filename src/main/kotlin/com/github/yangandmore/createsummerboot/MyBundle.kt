13273bce0e3ffefb28e3d18ad24247c89e901b1e
GitHub Action <action@github.com>
2022-06-21 03:24:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/yangandmore/createsummerboot/MyBundle.kt
+++ src/main/kotlin/com/github/yangandmore/createsummerboot/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yangandmore.createsummerboot
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/yangandmore/createsummerboot/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yangandmore.createsummerboot.services
+
+import com.github.yangandmore.createsummerboot.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
